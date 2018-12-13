c870ce4d1ae4bb08e7598b1fa3d27316ad5fa4a7
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2018-12-13 10:32:39 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much.";
+
+const token = '3a0f55d753c5d356c34920a7c74d15a08d4b4c57';
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+
+  .then(res => res.json())
+  .then(json => console.log(json))

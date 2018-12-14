c6804ad5705913b53039d8523db01411a41013b4
Andrew Jones <joneseyit@protonmail.com>
2018-12-14 13:08:04 -0500

    Read it

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 const app = "I don't do much.";
+
+fetch('https://api.github.com/repos/jquery/jquery/commits')
+  .then(res => res.json())
+  .then(json => console.log(json));
+
+const token = 'nada'  
